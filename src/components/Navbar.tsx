0912165c17e866112b8c16374233c608ed03b225
Ref-DevSa <ref.devsa@gmail.com>
2026-07-28 19:08:23 +0200

    Navbar created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <header className="bg-white shadow px-6 py-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold text-blue-600">
+        Project Dashboard
+      </h1>
+
+      <p className="text-gray-500">
+        Welcome Back 👋
+      </p>
+    </header>
+  );
+};
+
+export default Navbar;
