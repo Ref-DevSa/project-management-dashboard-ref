c8dec0cf953fc5b4a43e1f4f798081fdca290034
Ref-DevSa <ref.devsa@gmail.com>
2026-07-28 19:13:09 +0200

    Routes created and App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import MainLayout from "./layouts/MainLayout";
+
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import ProjectDetails from "./pages/ProjectDetails";
+import TaskDetails from "./pages/TaskDetails";
+
 function App() {
   return (
-    <h1>Project Management Dashboard</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route
+            path="/"
+            element={<Navigate to="/dashboard" replace />}
+          />
+
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/projects" element={<Projects />} />
+
+          <Route
+            path="/projects/:id"
+            element={<ProjectDetails />}
+          />
+
+          <Route
+            path="/tasks/:id"
+            element={<TaskDetails />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
